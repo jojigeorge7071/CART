651b85306add9a7902e9c671e028d9ec05c5fbb8
Joji George <gjojigeorge@gmail.com>
2023-03-11 04:54:52 +0530

    ready for cyclic hosting

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,11 @@ const state ={
     db:null
 }
 module.exports.connect =function(done){
-    const url ='mongodb+srv://username:username@cluster101.0ktl0i6.mongodb.net/?retryWrites=true&w=majority'
+    
+    // const url ='mongodb+srv://username:username@cluster101.0ktl0i6.mongodb.net/?retryWrites=true&w=majority'
+    // console.log(process.env.MONGO_URL+'MONGO_URL');
    // const url ='mongodb://localhost:27017'
+   const url= process.env.MONGO_URL
     const dbname = 'shopping'
 
     mongoClient.connect(url,(err,data)=>{
